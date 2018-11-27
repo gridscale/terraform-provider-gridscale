29f33cdb3171ffb589884727e8ca6ea16b64f81d
Wouter Wijsman <wouter@gridscale.io>
2018-11-27 14:35:00 +0100

    Updated provider_test.go

--- gridscale/provider_test.go
+++ gridscale/provider_test.go
@@ -1,13 +1,39 @@
 package gridscale
 
 import (
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/terraform"
 )
+
+var testAccProviders map[string]terraform.ResourceProvider
+var testAccProvider *schema.Provider
+
+func init() {
+	testAccProvider = Provider().(*schema.Provider)
+	testAccProviders = map[string]terraform.ResourceProvider{
+		"gridscale": testAccProvider,
+	}
+}
 
 func TestProvider(t *testing.T) {
 	if err := Provider().(*schema.Provider).InternalValidate(); err != nil {
 		t.Fatalf("err: %s", err)
+	}
+}
+
+func TestProvider_impl(t *testing.T) {
+	var _ terraform.ResourceProvider = Provider()
+}
+
+func testAccPreCheck(t *testing.T) {
+	if v := os.Getenv("GRIDSCALE_UUID"); v == "" {
+		t.Fatal("GRIDSCALE_UUID must be set for acceptance tests")
+	}
+
+	if v := os.Getenv("GRIDSCALE_TOKEN"); v == "" {
+		t.Fatal("GRIDSCALE_TOKEN must be set for acceptance tests")
 	}
 }
