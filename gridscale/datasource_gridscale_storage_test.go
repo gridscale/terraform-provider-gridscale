df5819eaf6a634154b35d51019a75140f51648e2
Ibrahim Takouna <ibrahim@gridscale.io>
2019-09-05 17:31:30 +0200

    Fix storage datasource and add test

---
+++ gridscale/datasource_gridscale_storage_test.go
@@ -0,0 +1,43 @@
+package gridscale
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccdataSourceGridscaleStorage_basic(t *testing.T) {
+	name := fmt.Sprintf("object-%s", acctest.RandString(10))
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckDataSourceGridscaleStorageDestroyCheck,
+		Steps: []resource.TestStep{
+			{
+
+				Config: testAccCheckDataSourceStorageConfig_basic(name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.gridscale_storage.foo", "id"),
+					resource.TestCheckResourceAttr("data.gridscale_storage.foo", "name", name),
+					resource.TestCheckResourceAttr("data.gridscale_storage.foo", "capacity", "1"),
+				),
+			},
+		},
+	})
+
+}
+
+func testAccCheckDataSourceStorageConfig_basic(name string) string {
+	return fmt.Sprintf(`
+
+resource "gridscale_storage" "foo" {
+  name   = "%s"
+  capacity = 1
+}
+
+data "gridscale_storage" "foo" {
+	resource_id   = "${gridscale_storage.foo.id}"
+}`, name)
+}
