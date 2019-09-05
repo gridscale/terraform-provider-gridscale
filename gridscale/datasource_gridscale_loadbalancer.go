8e916cea756cb8d136df8c15cc7b2bd555f7cc09
Ibrahim Takouna <ibrahim@gridscale.io>
2019-09-05 17:35:26 +0200

    Fix loadbalancer datasource and add test

--- gridscale/datasource_gridscale_loadbalancer.go
+++ gridscale/datasource_gridscale_loadbalancer.go
@@ -1,15 +1,99 @@
 package gridscale
 
 import (
-	"github.com/hashicorp/terraform/helper/schema"
+	"fmt"
 
 	"github.com/gridscale/gsclient-go"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/helper/validation"
 )
 
 func dataSourceGridscaleLoadBalancer() *schema.Resource {
 	return &schema.Resource{
-		Read:   dataSourceGridscaleLoadBalancerRead,
-		Schema: map[string]*schema.Schema{},
+		Read: dataSourceGridscaleLoadBalancerRead,
+
+		Schema: map[string]*schema.Schema{
+			"resource_id": &schema.Schema{
+				Type:         schema.TypeString,
+				Required:     true,
+				Description:  "ID of a resource",
+				ValidateFunc: validation.NoZeroValues,
+			},
+			"name": {
+				Type:        schema.TypeString,
+				Description: "The human-readable name of the object. It supports the full UTF-8 charset, with a maximum of 64 characters",
+				Computed:    true,
+			},
+			"location_uuid": {
+				Type:        schema.TypeString,
+				Description: "Helps to identify which datacenter an object belongs to.",
+				Computed:    true,
+			},
+			"algorithm": {
+				Type:        schema.TypeString,
+				Description: "The algorithm used to process requests. Accepted values: roundrobin/leastconn.",
+				Computed:    true},
+			"forwarding_rule": {
+				Type:        schema.TypeSet,
+				Description: "List of forwarding rules for the Load balancer.",
+				Computed:    true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"letsencrypt_ssl": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"listen_port": {
+							Type:     schema.TypeInt,
+							Computed: true},
+						"mode": {
+							Type:     schema.TypeString,
+							Computed: true},
+						"target_port": {
+							Type:     schema.TypeInt,
+							Computed: true},
+					},
+				},
+			},
+			"backend_server": {
+				Type:        schema.TypeSet,
+				Description: "List of backend servers.",
+				Computed:    true, Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"weight": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"host": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"status": {
+				Type:        schema.TypeString,
+				Description: "Status indicates the status of the object.",
+				Computed:    true,
+			},
+			"redirect_http_to_https": {
+				Type:        schema.TypeBool,
+				Description: "Whether the Load balancer is forced to redirect requests from HTTP to HTTPS",
+				Computed:    true},
+			"labels": {
+				Type:        schema.TypeSet,
+				Description: "List of labels.",
+				Computed:    true, Elem: &schema.Schema{Type: schema.TypeString},
+			},
+			"listen_ipv4_uuid": {
+				Type:        schema.TypeString,
+				Description: "The UUID of the IPv4 address the Load balancer will listen to for incoming requests.",
+				Computed:    true},
+			"listen_ipv6_uuid": {
+				Type:        schema.TypeString,
+				Description: "The UUID of the IPv6 address the Load balancer will listen to for incoming requests.",
+				Computed:    true},
+		},
 	}
 }
 
@@ -20,6 +104,28 @@ func dataSourceGridscaleLoadBalancerRead(d *schema.ResourceData, meta interface{
 
 	if err == nil {
 		d.SetId(loadbalancer.Properties.ObjectUuid)
+		d.Set("name", loadbalancer.Properties.Name)
+		d.Set("algorithm", loadbalancer.Properties.Algorithm)
+		d.Set("status", loadbalancer.Properties.Status)
+		d.Set("redirect_http_to_https", loadbalancer.Properties.RedirectHTTPToHTTPS)
+		d.Set("listen_ipv4_uuid", loadbalancer.Properties.ListenIPv4Uuid)
+		d.Set("listen_ipv6_uuid", loadbalancer.Properties.ListenIPv6Uuid)
+		if loadbalancer.Properties.ForwardingRules != nil {
+			err = d.Set("forwarding_rule", flattenLoadbalancerForwardingRules(loadbalancer.Properties.ForwardingRules))
+			if err != nil {
+				return err
+			}
+		}
+		if loadbalancer.Properties.BackendServers != nil {
+			err = d.Set("backend_server", flattenLoadbalancerBackendServers(loadbalancer.Properties.BackendServers))
+
+			if err != nil {
+				return err
+			}
+		}
+		if err = d.Set("labels", loadbalancer.Properties.Labels); err != nil {
+			return fmt.Errorf("Error setting labels: %v", err)
+		}
 	}
 
 	return err
