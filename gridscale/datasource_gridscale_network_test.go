3077f8300ed0decbf1304eda5599ebe6fbea2b2d
Ibrahim Takouna <ibrahim@gridscale.io>
2019-09-05 17:36:57 +0200

    Fix network datasource and add test

---
+++ gridscale/datasource_gridscale_network_test.go
@@ -0,0 +1,41 @@
+package gridscale
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccdataSourceGridscaleNetwork_basic(t *testing.T) {
+	name := fmt.Sprintf("object-%s", acctest.RandString(10))
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckDataSourceGridscaleNetworkDestroyCheck,
+		Steps: []resource.TestStep{
+			{
+
+				Config: testAccCheckDataSourceNetworkConfig_basic(name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.gridscale_network.foo", "id"),
+					resource.TestCheckResourceAttr("data.gridscale_network.foo", "name", name),
+				),
+			},
+		},
+	})
+
+}
+
+func testAccCheckDataSourceNetworkConfig_basic(name string) string {
+	return fmt.Sprintf(`
+
+resource "gridscale_network" "foo" {
+  name   = "%s"
+}
+
+data "gridscale_network" "foo" {
+	resource_id   = "${gridscale_network.foo.id}"
+}`, name)
+}
