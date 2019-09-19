5dddd9b95656462a6969ab713fa8f464e3890680
Tom <nvthongswansea@gmail.com>
2019-09-19 13:38:12 +0200

    Add function converting `[]interface{}` to `[]string`

--- gridscale/common.go
+++ gridscale/common.go
@@ -1,1 +1,10 @@
 package gridscale
+
+//convSOStrings converts slice of interfaces to slice of strings
+func convSOStrings(interfaceList []interface{}) []string {
+	var labels []string
+	for _, labelInterface := range interfaceList {
+		labels = append(labels, labelInterface.(string))
+	}
+	return labels
+}
