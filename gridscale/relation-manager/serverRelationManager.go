580c9da5e2afe62e5fb93b5738d18cbe051583fc
nvthongswansea <nvthongswansea@gmail.com>
2019-10-24 13:52:26 +0200

    add server dependency manager
    Server dependency manager features:
    - Create/Update/Remove server's realtions.

---
+++ gridscale/relation-manager/serverRelationManager.go
@@ -0,0 +1,377 @@
+package relation_manager
+
+import (
+	"context"
+	"fmt"
+	"github.com/gridscale/gsclient-go"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"net/http"
+)
+
+//firewallRuleTypes defines all types of firewall rules
+var firewallRuleTypes = []string{"rules_v4_in", "rules_v4_out", "rules_v6_in", "rules_v6_out"}
+
+//ServerRelationManger is an wrapper of gsclient which is used for
+//managing relations of a server in gridscale terraform provider
+type ServerRelationManger struct {
+	gsc  *gsclient.Client
+	data *schema.ResourceData
+}
+
+//NewServerDepClient creates a new instance DependencyClient
+func NewServerDepClient(gsc *gsclient.Client, d *schema.ResourceData) *ServerRelationManger {
+	return &ServerRelationManger{gsc, d}
+}
+
+//getGSClient returns gsclient from server relation manager
+func (c ServerRelationManger) getGSClient() *gsclient.Client {
+	return c.gsc
+}
+
+//getData returns resource data from server relation manager
+func (c ServerRelationManger) getData() *schema.ResourceData {
+	return c.data
+}
+
+//LinkStorages links storages to a server
+//**Note: if no storage is set as the boot device. The storage in the
+//1st order of the list will be set as the boot device automatically
+func (c *ServerRelationManger) LinkStorages(ctx context.Context) error {
+	d := c.getData()
+	client := c.getGSClient()
+	if attr, ok := d.GetOk("storage"); ok {
+		for _, value := range attr.([]interface{}) {
+			storage := value.(map[string]interface{})
+			err := client.LinkStorage(ctx, d.Id(), storage["object_uuid"].(string), storage["bootdevice"].(bool))
+			if err != nil {
+				return fmt.Errorf(
+					"Error waiting for storage (%s) to be attached to server (%s): %s",
+					storage["object_uuid"].(string),
+					d.Id(),
+					err,
+				)
+			}
+		}
+	}
+	return nil
+}
+
+//LinkIPv4 links IPv4 address to a server
+func (c *ServerRelationManger) LinkIPv4(ctx context.Context) error {
+	d := c.getData()
+	client := c.getGSClient()
+	if attr, ok := d.GetOk("ipv4"); ok {
+		//Check IP version
+		if client.GetIPVersion(ctx, attr.(string)) != 4 {
+			return fmt.Errorf("The IP address with UUID %v is not version 4", attr.(string))
+		}
+		err := client.LinkIP(ctx, d.Id(), attr.(string))
+		if err != nil {
+			return fmt.Errorf(
+				"Error waiting for IP address (%s) to be attached to server (%s): %s",
+				attr.(string),
+				d.Id(),
+				err,
+			)
+		}
+	}
+	return nil
+}
+
+//LinkIPv6 link an IPv6 address to a server
+func (c *ServerRelationManger) LinkIPv6(ctx context.Context) error {
+	d := c.getData()
+	client := c.getGSClient()
+	if attr, ok := d.GetOk("ipv6"); ok {
+		//Check IP version
+		if client.GetIPVersion(ctx, attr.(string)) != 6 {
+			return fmt.Errorf("The IP address with UUID %v is not version 6", attr.(string))
+		}
+		err := client.LinkIP(ctx, d.Id(), attr.(string))
+		if err != nil {
+			return fmt.Errorf(
+				"Error waiting for IP address (%s) to be attached to server (%s): %s",
+				attr.(string),
+				d.Id(),
+				err,
+			)
+		}
+	}
+	return nil
+}
+
+//LinkISOImage links an ISO-image to a server
+func (c *ServerRelationManger) LinkISOImage(ctx context.Context) error {
+	d := c.getData()
+	client := c.getGSClient()
+	if attr, ok := d.GetOk("isoimage"); ok {
+		err := client.LinkIsoImage(ctx, d.Id(), attr.(string))
+		if err != nil {
+			return fmt.Errorf(
+				"Error waiting for iso-image (%s) to be attached to server (%s): %s",
+				attr.(string),
+				d.Id(),
+				err,
+			)
+		}
+	}
+	return nil
+}
+
+//LinkNetworks links networks to server
+func (c *ServerRelationManger) LinkNetworks(ctx context.Context) error {
+	d := c.getData()
+	client := c.getGSClient()
+	if attrNetRel, ok := d.GetOk("network"); ok {
+		for _, value := range attrNetRel.(*schema.Set).List() {
+			network := value.(map[string]interface{})
+			//Read custom firewall rules from `network` property (field)
+			customFwRules := readCustomFirewallRules(network)
+			err := client.LinkNetwork(
+				ctx,
+				d.Id(),
+				network["object_uuid"].(string),
+				"",
+				network["bootdevice"].(bool),
+				0,
+				nil,
+				&customFwRules,
+			)
+			if err != nil {
+				return fmt.Errorf(
+					"Error waiting for network (%s) to be attached to server (%s): %s",
+					network["object_uuid"],
+					d.Id(),
+					err,
+				)
+			}
+		}
+	}
+	return nil
+}
+
+//readCustomFirewallRules reads custom firewall rules from a specific network
+//returns `gsclient.FirewallRules` type variable
+func readCustomFirewallRules(netData map[string]interface{}) gsclient.FirewallRules {
+	//Init firewall rule variable
+	var fwRules gsclient.FirewallRules
+
+	//Loop through all firewall rule types
+	//there are 4 types: "rules_v4_in", "rules_v4_out", "rules_v6_in", "rules_v6_out".
+	for _, ruleType := range firewallRuleTypes {
+		//Init array of firewall rules
+		var rules []gsclient.FirewallRuleProperties
+		//Check if the firewall rule type is declared in the current network
+		if rulesInTypeAttr, ok := netData[ruleType]; ok {
+			//Loop through all rules in the current firewall type
+			for _, rulesInType := range rulesInTypeAttr.([]interface{}) {
+				ruleProps := rulesInType.(map[string]interface{})
+				ruleProperties := gsclient.FirewallRuleProperties{
+					Protocol: ruleProps["protocol"].(string),
+					DstPort:  ruleProps["dst_port"].(string),
+					SrcPort:  ruleProps["src_port"].(string),
+					SrcCidr:  ruleProps["src_cidr"].(string),
+					Action:   ruleProps["action"].(string),
+					Comment:  ruleProps["comment"].(string),
+					DstCidr:  ruleProps["dst_cidr"].(string),
+					Order:    ruleProps["order"].(int),
+				}
+				//Add rule to the array of rules
+				rules = append(rules, ruleProperties)
+			}
+		}
+
+		//Based on rule type to place the rules in the right property of fwRules variable
+		if ruleType == "rules_v4_in" {
+			fwRules.RulesV4In = rules
+		} else if ruleType == "rules_v4_out" {
+			fwRules.RulesV4Out = rules
+		} else if ruleType == "rules_v6_in" {
+			fwRules.RulesV6In = rules
+		} else if ruleType == "rules_v6_out" {
+			fwRules.RulesV6Out = rules
+		}
+	}
+	return fwRules
+}
+
+//IsShutdownRequired checks if server is needed to be shutdown when updating
+func (c *ServerRelationManger) IsShutdownRequired(ctx context.Context) bool {
+	var shutdownRequired bool
+	d := c.getData()
+	//If the number of cores is decreased, shutdown the server
+	if d.HasChange("cores") {
+		old, new := d.GetChange("cores")
+		if new.(int) < old.(int) || d.Get("legacy").(bool) { //Legacy systems don't support updating the memory while running
+			shutdownRequired = true
+		}
+	}
+	//If the amount of memory is decreased, shutdown the server
+	if d.HasChange("memory") {
+		old, new := d.GetChange("memory")
+		if new.(int) < old.(int) || d.Get("legacy").(bool) { //Legacy systems don't support updating the memory while running
+			shutdownRequired = true
+		}
+	}
+	//If IP address, storages, or networks are changed, shutdown the server
+	if d.HasChange("ipv4") || d.HasChange("ipv6") || d.HasChange("storage") || d.HasChange("network") {
+		shutdownRequired = true
+	}
+	return shutdownRequired
+}
+
+//UpdateISOImageRel updates relationship between a server and an ISO-image
+func (c *ServerRelationManger) UpdateISOImageRel(ctx context.Context) error {
+	d := c.getData()
+	client := c.getGSClient()
+	var err error
+	//Check if ISO-image field is changed
+	if d.HasChange("isoimage") {
+		oldIso, _ := d.GetChange("isoimage")
+		//If there is an ISO-image already linked to the server
+		//Unlink it
+		if oldIso != "" {
+			//If 404 or 409, that means ISO-image is already deleted => the relation between ISO-image and server is deleted automatically
+			err = removeErrorContainsHttpCodes(
+				client.UnlinkIsoImage(ctx, d.Id(), oldIso.(string)),
+				http.StatusConflict,
+				http.StatusNotFound,
+			)
+			if err != nil {
+				return err
+			}
+		}
+		//Link new ISO-image (if there is one)
+		err = c.LinkISOImage(ctx)
+	}
+	return err
+}
+
+//UpdateIPv4Rel updates relationship between a server and an IPv4 address
+func (c *ServerRelationManger) UpdateIPv4Rel(ctx context.Context) error {
+	d := c.getData()
+	client := c.getGSClient()
+	var err error
+	//If IPv4 field is changed
+	if d.HasChange("ipv4") {
+		oldIp, _ := d.GetChange("ipv4")
+		//If there is an IPv4 address already linked to the server
+		//Unlink it
+		if oldIp != "" {
+			//If 404 or 409, that means IP is already deleted => the relation between IP and server is deleted automatically
+			err = removeErrorContainsHttpCodes(
+				client.UnlinkIP(ctx, d.Id(), oldIp.(string)),
+				http.StatusConflict,
+				http.StatusNotFound,
+			)
+			if err != nil {
+				return err
+			}
+		}
+		//Link new IPv4 (if there is one)
+		err = c.LinkIPv4(ctx)
+	}
+	return err
+}
+
+//UpdateIPv6Rel updates relationship between a server and an IPv6 address
+func (c *ServerRelationManger) UpdateIPv6Rel(ctx context.Context) error {
+	d := c.getData()
+	client := c.getGSClient()
+	var err error
+	if d.HasChange("ipv6") {
+		oldIp, _ := d.GetChange("ipv6")
+		//If there is an IPv6 address already linked to the server
+		//Unlink it
+		if oldIp != "" {
+			//If 404 or 409, that means IP is already deleted => the relation between IP and server is deleted automatically
+			err = removeErrorContainsHttpCodes(
+				client.UnlinkIP(ctx, d.Id(), oldIp.(string)),
+				http.StatusConflict,
+				http.StatusNotFound,
+			)
+			if err != nil {
+				return err
+			}
+		}
+		//Link new IPv6 (if there is one)
+		err = c.LinkIPv6(ctx)
+	}
+	return err
+}
+
+//UpdateNetworksRel updates relationship between a server and networks
+func (c *ServerRelationManger) UpdateNetworksRel(ctx context.Context) error {
+	d := c.getData()
+	client := c.getGSClient()
+	var err error
+	if d.HasChange("network") {
+		oldNetworks, _ := d.GetChange("network")
+		//Unlink all old networks if there are any networks linked to the server
+		for _, value := range oldNetworks.(*schema.Set).List() {
+			network := value.(map[string]interface{})
+			if network["object_uuid"].(string) != "" {
+				//If 404 or 409, that means network is already deleted => the relation between network and server is deleted automatically
+				err = removeErrorContainsHttpCodes(
+					client.UnlinkNetwork(ctx, d.Id(), network["object_uuid"].(string)),
+					http.StatusConflict,
+					http.StatusNotFound,
+				)
+				if err != nil {
+					return err
+				}
+			}
+		}
+		//Links all new networks (if there are some)
+		err = c.LinkNetworks(ctx)
+	}
+	return err
+}
+
+//UpdateStoragesRel updates relationship between a server and storages
+func (c *ServerRelationManger) UpdateStoragesRel(ctx context.Context) error {
+	d := c.getData()
+	client := c.getGSClient()
+	var err error
+	if d.HasChange("storage") {
+		oldStorages, _ := d.GetChange("storage")
+		for _, value := range oldStorages.([]interface{}) {
+			storage := value.(map[string]interface{})
+			if storage["object_uuid"].(string) != "" {
+				//If 404 or 409, that means storage is already deleted => the relation between storage and server is deleted automatically
+				err = removeErrorContainsHttpCodes(
+					client.UnlinkStorage(ctx, d.Id(), storage["object_uuid"].(string)),
+					http.StatusConflict,
+					http.StatusNotFound,
+				)
+				if err != nil {
+					return err
+				}
+			}
+		}
+		//Links all new storages (if there are some)
+		err = c.LinkStorages(ctx)
+	}
+	return err
+}
+
+//removeErrorContainsHttpCodes returns nil, if the error of HTTP error
+//has status code that is in the given list of http status codes
+func removeErrorContainsHttpCodes(err error, errorCodes ...int) error {
+	if requestError, ok := err.(gsclient.RequestError); ok {
+		if containsInt(errorCodes, requestError.StatusCode) {
+			err = nil
+		}
+	}
+	return err
+}
+
+//containsInt check if an int array contains a specific int.
+func containsInt(arr []int, target int) bool {
+	for _, a := range arr {
+		if a == target {
+			return true
+		}
+	}
+	return false
+}
