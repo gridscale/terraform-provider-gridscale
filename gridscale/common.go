c20cef5137a3d2aafe7f67d2e5b6e48182ff63ba
nvthongswansea <nvthongswansea@gmail.com>
2019-10-24 13:57:15 +0200

    add sync feature when update power state of server

--- gridscale/common.go
+++ gridscale/common.go
@@ -1,5 +1,150 @@
 package gridscale
 
+import (
+	"context"
+	"errors"
+	"github.com/gridscale/gsclient-go"
+	"log"
+	"sync"
+)
+
+//serverPowerStatus represents power state of a server
+//mutex is used to lock the resource when a goroutine changes a server's
+//power state, so it prevents other goroutines from accessing/modifying the server's power state
+type serverPowerStatus struct {
+	power bool
+	mux   sync.Mutex
+}
+
+//listServersPowerStatus represents a list of power states of
+//all servers (declared in terraform).
+//mutex is used to lock when adding or removing servers.
+//***NOTE: servers declared outside terraform are not included.
+type listServersPowerStatus struct {
+	list map[string]*serverPowerStatus
+	mux  sync.Mutex
+}
+
+//actionRequireServerOff signature of a function that requires a server to be off
+//in order to run
+type actionRequireServerOff func() error
+
+//addServer adds a server power state to the list
+func (l *listServersPowerStatus) addServer(id string) {
+	//lock the list
+	l.mux.Lock()
+	log.Printf("[DEBUG] LOCK ACQUIRED to add server (%v)", id)
+	defer func() {
+		//unlock the list
+		l.mux.Unlock()
+		log.Printf("[DEBUG] LOCK RELEASED! Server (%v) is added", id)
+	}()
+	l.list[id] = &serverPowerStatus{
+		false,
+		sync.Mutex{},
+	}
+}
+
+//removeServer removes a server power state from the list
+func (l *listServersPowerStatus) removeServer(id string) error {
+	//lock the list
+	l.mux.Lock()
+	log.Printf("[DEBUG] LOCK ACQUIRED to remove server (%v)", id)
+	defer func() {
+		//unlock the list
+		l.mux.Unlock()
+		log.Printf("[DEBUG] LOCK RELEASED! Server (%v) is removed", id)
+	}()
+	if _, ok := l.list[id]; ok {
+		delete(l.list, id)
+		return nil
+	}
+	return errors.New("server does not exist in current list of servers in terraform")
+}
+
+//getServerPowerStatus returns power state of a server in the list
+func (l *listServersPowerStatus) getServerPowerStatus(id string) (bool, error) {
+	//lock the power state of the server
+	l.list[id].mux.Lock()
+	log.Printf("[DEBUG] LOCK ACQUIRED to get server (%v) power status", id)
+	defer func() {
+		//unlock the power state of the server after retrieving server's power state
+		l.list[id].mux.Unlock()
+		log.Printf("[DEBUG] LOCK RELEASED! Getting server (%v) power status is done", id)
+	}()
+	if _, ok := l.list[id]; ok {
+		return l.list[id].power, nil
+	}
+	return false, errors.New("server does not exist in current list of servers in terraform")
+}
+
+func (l *listServersPowerStatus) enhancedStartServer(ctx context.Context, c *gsclient.Client, id string) error {
+	//lock the power state of the server
+	l.list[id].mux.Lock()
+	log.Printf("[DEBUG] LOCK ACQUIRED to start server (%v)", id)
+	defer func() {
+		//unlock the power state of the server after server is started
+		l.list[id].mux.Unlock()
+		log.Printf("[DEBUG] LOCK RELEASED! Server (%v) is started", id)
+	}()
+	if _, ok := l.list[id]; ok {
+		err := c.StartServer(ctx, id)
+		if err != nil {
+			return err
+		}
+		l.list[id].power = true
+		return nil
+	}
+	return errors.New("server does not exist in current list of serversin terraform")
+}
+
+func (l *listServersPowerStatus) enhancedStopServer(ctx context.Context, c *gsclient.Client, id string) error {
+	//lock the power state of the server
+	l.list[id].mux.Lock()
+	log.Printf("[DEBUG] LOCK ACQUIRED to stop server (%v)", id)
+	defer func() {
+		//unlock the power state of the server after server is stopped
+		l.list[id].mux.Unlock()
+		log.Printf("[DEBUG] LOCK RELEASED! Server (%v) is stopped", id)
+	}()
+	if _, ok := l.list[id]; ok {
+		err := c.StopServer(ctx, id)
+		if err != nil {
+			return err
+		}
+		l.list[id].power = false
+		return nil
+	}
+	return errors.New("server does not exist in current list of servers")
+}
+
+func (l *listServersPowerStatus) runActionRequireServerOff(ctx context.Context, c *gsclient.Client, id string, action actionRequireServerOff) error {
+	//lock the power state of the server
+	l.list[id].mux.Lock()
+	log.Printf("[DEBUG] LOCK ACQUIRED to run an action requiring server (%v) to be OFF", id)
+	defer func() {
+		//unlock the power state of the server after action is done
+		l.list[id].mux.Unlock()
+		log.Printf("[DEBUG] LOCK RELEASED! Action requiring server (%v) is done", id)
+	}()
+	if _, ok := l.list[id]; ok {
+		err := c.StopServer(ctx, id)
+		if err != nil {
+			return err
+		}
+		l.list[id].power = false
+		//run action function
+		err = action()
+		return err
+	}
+	return errors.New("server does not exist in current list of servers")
+}
+
+//currentServersPowerStatus global list of all servers' power states in terraform
+var currentServersPowerStatus = listServersPowerStatus{
+	list: make(map[string]*serverPowerStatus),
+}
+
 //convSOStrings converts slice of interfaces to slice of strings
 func convSOStrings(interfaceList []interface{}) []string {
 	var labels []string
