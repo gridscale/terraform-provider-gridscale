9a9b63919d99af51b5eef028272cba549574b7ef
Van Thong Nguyen <tom@gridscale.io>
2019-11-12 23:53:49 +0100

    load/save `serverPowerStateList` data from/to file
    `serverPowerStateList`'s data will be loaded from when the provider start and will be saved to file when the provider exits.

--- main.go
+++ main.go
@@ -5,7 +5,22 @@ import (
 	"github.com/terraform-providers/terraform-provider-gridscale/gridscale"
 )
 
+const (
+	serverStateFile = "./server.gsstate"
+)
+
 func main() {
+	serverList := gridscale.NewGlobalServerPowerStateList()
+	err := serverList.LoadFromFile(serverStateFile)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		err := serverList.FlushToFile(serverStateFile)
+		if err != nil {
+			panic(err)
+		}
+	}()
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: gridscale.Provider})
 }
