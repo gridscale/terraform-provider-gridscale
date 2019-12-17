a89aee4c703914868714ea04426aebaf2ad9ca4b
Van Thong Nguyen <tom@gridscale.io>
2019-12-17 13:29:01 +0100

    add pub net datasource's acceptance test

---
+++ gridscale/datasource_gridscale_publicnetwork_test.go
@@ -0,0 +1,32 @@
+package gridscale
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccdataSourceGridscalePublicNetwork_basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+
+				Config: testAccCheckDataSourcePublicNetworkConfig_basic(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.gridscale_public_network.foo", "id"),
+					resource.TestCheckResourceAttrSet("data.gridscale_public_network.foo", "name"),
+				),
+			},
+		},
+	})
+
+}
+
+func testAccCheckDataSourcePublicNetworkConfig_basic() string {
+	return fmt.Sprint(`
+data "gridscale_public_network" "foo" {
+}`)
+}
