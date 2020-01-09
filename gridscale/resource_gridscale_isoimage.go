885d99488d7c8cd1c70af15bf2e1a3e4dda88871
Van Thong Nguyen <tom@gridscale.io>
2020-01-09 14:16:58 +0100

    add read func and remove `license_product_no` property

--- gridscale/resource_gridscale_isoimage.go
+++ gridscale/resource_gridscale_isoimage.go
@@ -1,11 +1,14 @@
 package gridscale
 
 import (
+	"fmt"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/terraform-providers/terraform-provider-gridscale/vendor/github.com/gridscale/gsclient-go"
 )
 
 func resourceGridscaleISOImage() *schema.Resource {
 	return &schema.Resource{
+		Read: resourceGridscaleISOImageRead,
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
 		},
@@ -56,11 +59,6 @@ func resourceGridscaleISOImage() *schema.Resource {
 				Description: "The object is private, the value will be true. Otherwise the value will be false.",
 				Computed:    true,
 			},
-			"license_product_no": {
-				Type:        schema.TypeInt,
-				Description: "If a template has been used that requires a license key (e.g. Windows Servers) this shows the product_no of the license (see the /prices endpoint for more details).",
-				Computed:    true,
-			},
 			"create_time": {
 				Type:        schema.TypeString,
 				Description: "The date and time the object was initially created.",
@@ -99,4 +97,40 @@ func resourceGridscaleISOImage() *schema.Resource {
 			},
 		},
 	}
+}
+
+func resourceGridscaleISOImageRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*gsclient.Client)
+	iso, err := client.GetISOImage(emptyCtx, d.Id())
+	if err != nil {
+		if requestError, ok := err.(gsclient.RequestError); ok {
+			if requestError.StatusCode == 404 {
+				d.SetId("")
+				return nil
+			}
+		}
+		return err
+	}
+	props := iso.Properties
+	d.Set("name", props.Name)
+	d.Set("source_url", props.SourceURL)
+	d.Set("location_uuid", props.LocationUUID)
+	d.Set("location_country", props.LocationCountry)
+	d.Set("location_iata", props.LocationIata)
+	d.Set("location_name", props.LocationName)
+	d.Set("status", props.Status)
+	d.Set("version", props.Version)
+	d.Set("private", props.Private)
+	d.Set("create_time", props.CreateTime)
+	d.Set("change_time", props.ChangeTime)
+	d.Set("description", props.Description)
+	d.Set("usage_in_minutes", props.UsageInMinutes)
+	d.Set("capacity", props.Capacity)
+	d.Set("current_price", props.CurrentPrice)
+
+	if err = d.Set("labels", props.Labels); err != nil {
+		return fmt.Errorf("Error setting labels: %v", err)
+	}
+
+	return nil
 }
