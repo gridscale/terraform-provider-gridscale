b2f5c331c023a95eef57fec0e2e0273bca7c980e
Van Thong Nguyen <tom@gridscale.io>
2020-01-14 22:00:52 +0100

    replace "isoimage" with "ISO Image"

--- gridscale/resource_gridscale_isoimage.go
+++ gridscale/resource_gridscale_isoimage.go
@@ -31,7 +31,7 @@ func resourceGridscaleISOImage() *schema.Resource {
 			},
 			"server": {
 				Type:        schema.TypeSet,
-				Description: "The information about servers which are related to this isoimage.",
+				Description: "The information about servers which are related to this ISO image.",
 				Computed:    true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
