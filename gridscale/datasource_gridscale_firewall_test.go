89daa0a118e47f37a36ba07c5bf570f3fd6cdfb3
Van Thong Nguyen <tom@gridscale.io>
2020-01-14 12:24:53 +0100

    add fw datasource's acceptance test

---
+++ gridscale/datasource_gridscale_firewall_test.go
@@ -0,0 +1,57 @@
+package gridscale
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccdataSourceGridscaleFirewall_basic(t *testing.T) {
+	name := fmt.Sprintf("object-%s", acctest.RandString(10))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckGridscaleIpv4DestroyCheck,
+		Steps: []resource.TestStep{
+			{
+
+				Config: testAccCheckDataSourceFirewallConfig_basic(name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.gridscale_firewall.foo", "id"),
+					resource.TestCheckResourceAttr("data.gridscale_firewall.foo", "name", name),
+				),
+			},
+		},
+	})
+
+}
+
+func testAccCheckDataSourceFirewallConfig_basic(name string) string {
+	return fmt.Sprintf(`
+resource "gridscale_firewall" "foo" {
+  name   = "%s"
+  rules_v4_in {
+	order = 0
+	protocol = "tcp"
+	action = "drop"
+	dst_port = "20:80"
+	comment = "test"
+  }
+  rules_v6_in {
+	order = 0
+	protocol = "tcp"
+	action = "drop"
+	dst_port = "2000:3000"
+	comment = "testv6"
+  }
+}
+
+data "gridscale_firewall" "foo" {
+	resource_id   = gridscale_firewall.foo.id
+}
+
+`, name)
+}
