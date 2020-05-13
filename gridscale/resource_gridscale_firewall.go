da719ab78804351a3ecd9f32ed274bdefb67b256
Van Thong Nguyen <tom@gridscale.io>
2020-05-13 20:47:55 +0200

    fix importing wrong path of `error_handler` package

--- gridscale/resource_gridscale_firewall.go
+++ gridscale/resource_gridscale_firewall.go
@@ -11,7 +11,7 @@ import (
 	"github.com/gridscale/gsclient-go/v3"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
-	errHandler "github.com/terraform-providers/terraform-provider-gridscale/gridscale/error_handler"
+	errHandler "github.com/terraform-providers/terraform-provider-gridscale/gridscale/error-handler"
 )
 
 func resourceGridscaleFirewall() *schema.Resource {
