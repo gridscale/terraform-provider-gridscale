d1b07fbe1c32c8c821d1d00b51dda11baaea9b34
Van Thong Nguyen <tom@gridscale.io>
2020-09-03 19:07:32 +0200

    add backup datasource's acceptance test

---
+++ gridscale/datasource_gridscale_backup_schedule_test.go
@@ -0,0 +1,52 @@
+package gridscale
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccdataSourceGridscaleBackupSchedule_basic(t *testing.T) {
+	name := fmt.Sprintf("object-%s", acctest.RandString(10))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckDataSourceGridscaleBackupScheduleDestroyCheck,
+		Steps: []resource.TestStep{
+			{
+
+				Config: testAccCheckDataSourceBackupScheduleConfig_basic(name),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.gridscale_backupschedule.foo", "id"),
+					resource.TestCheckResourceAttr("data.gridscale_backupschedule.foo", "name", name),
+				),
+			},
+		},
+	})
+
+}
+
+func testAccCheckDataSourceBackupScheduleConfig_basic(name string) string {
+	return fmt.Sprintf(`
+
+resource "gridscale_storage" "foo" {
+  name   = "storage"
+  capacity = 1
+}
+resource "gridscale_backupschedule" "foo" {
+  name = "%s"
+  storage_uuid = gridscale_storage.foo.id
+  keep_backups = 1
+  run_interval = 60
+  next_runtime = "2025-12-30 15:04:05"
+  active 	   = true
+}
+data "gridscale_backupschedule" "foo" {
+	resource_id   = gridscale_backupschedule.foo.id
+	storage_uuid   = gridscale_storage.foo.id
+}
+`, name)
+}
