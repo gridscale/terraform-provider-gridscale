7a0cfd704e664511150592f4887746b6b6febd70
Van Thong Nguyen <tom@gridscale.io>
2020-09-04 23:06:05 +0200

    add gridscale_marketplace_application_import + acceptance test

---
+++ gridscale/resource_gridscale_marketplace_app_import.go
@@ -0,0 +1,193 @@
+package gridscale
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/gridscale/gsclient-go/v3"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func resourceGridscaleImportedMarketplaceApplication() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceGridscaleMarketplaceApplicationImport,
+		Read:   resourceGridscaleMarketplaceApplicationRead,
+		Delete: resourceGridscaleMarketplaceApplicationDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"import_unique_hash": {
+				Type:        schema.TypeString,
+				Description: "Hash of a specific marketplace application that you want to import",
+				Required:    true,
+				ForceNew:    true,
+			},
+			"unique_hash": {
+				Type:        schema.TypeString,
+				Description: "Unique hash to allow user to import the self-created marketplace application",
+				Computed:    true,
+			},
+			"name": {
+				Type:        schema.TypeString,
+				Description: "The human-readable name of the object. It supports the full UTF-8 charset, with a maximum of 64 characters",
+				Computed:    true,
+			},
+			"category": {
+				Type:        schema.TypeString,
+				Description: "Category of marketplace application. Accepted values: \"CMS\", \"project management\", \"Adminpanel\", \"Collaboration\", \"Cloud Storage\", \"Archiving\"",
+				Computed:    true,
+			},
+			"object_storage_path": {
+				Type:        schema.TypeString,
+				Description: "Path to the images for the application, must be in .gz format and started with s3//",
+				Computed:    true,
+			},
+			"setup_cores": {
+				Type:        schema.TypeInt,
+				Description: "Number of server's cores",
+				Computed:    true,
+			},
+			"setup_memory": {
+				Type:        schema.TypeInt,
+				Description: "The capacity of server's memory in GB",
+				Computed:    true,
+			},
+			"setup_storage_capacity": {
+				Type:        schema.TypeInt,
+				Description: "The capacity of server's storage in GB",
+				Computed:    true,
+			},
+			"meta_license": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"meta_os": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"meta_components": {
+				Type:     schema.TypeSet,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"meta_overview": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"meta_hints": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"meta_terms_of_use": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"meta_icon": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"meta_features": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"meta_author": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"meta_advices": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"is_application_owner": {
+				Type:        schema.TypeBool,
+				Description: "Whether the you are the owner of application or not",
+				Computed:    true,
+			},
+			"is_published": {
+				Type:        schema.TypeBool,
+				Description: "Whether the template is published by the partner to their tenant",
+				Computed:    true,
+			},
+			"published_date": {
+				Type:        schema.TypeString,
+				Description: "The date when the template is published into other tenant in the same partner",
+				Computed:    true,
+			},
+			"is_publish_requested": {
+				Type:        schema.TypeBool,
+				Description: "Whether the tenants want their template to be published or not",
+				Computed:    true,
+			},
+			"publish_requested_date": {
+				Type:        schema.TypeString,
+				Description: "The date when the tenant requested their template to be published",
+				Computed:    true,
+			},
+			"is_publish_global_requested": {
+				Type:        schema.TypeBool,
+				Description: "Whether a partner wants their tenant template published to other partners",
+				Computed:    true,
+			},
+			"publish_global_requested_date": {
+				Type:        schema.TypeString,
+				Description: "The date when a partner requested their tenants template to be published",
+				Computed:    true,
+			},
+			"is_publish_global": {
+				Type:        schema.TypeBool,
+				Description: "Whether a template is published to other partner or not",
+				Computed:    true,
+			},
+			"published_global_date": {
+				Type:        schema.TypeString,
+				Description: "The date when a template is published to other partner",
+				Computed:    true,
+			},
+			"type": {
+				Type:        schema.TypeString,
+				Description: "The type of template",
+				Computed:    true,
+			},
+			"status": {
+				Type:        schema.TypeString,
+				Description: "status indicates the status of the object",
+				Computed:    true,
+			},
+			"create_time": {
+				Type:        schema.TypeString,
+				Description: "Defines the date and time the object was initially created",
+				Computed:    true,
+			},
+			"change_time": {
+				Type:        schema.TypeString,
+				Description: "Defines the date and time of the last object change",
+				Computed:    true,
+			},
+		},
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(5 * time.Minute),
+			Update: schema.DefaultTimeout(5 * time.Minute),
+			Delete: schema.DefaultTimeout(5 * time.Minute),
+		},
+	}
+}
+
+func resourceGridscaleMarketplaceApplicationImport(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*gsclient.Client)
+	ctx, cancel := context.WithTimeout(context.Background(), d.Timeout(schema.TimeoutCreate))
+	defer cancel()
+	response, err := client.ImportMarketplaceApplication(ctx, gsclient.MarketplaceApplicationImportRequest{
+		UniqueHash: d.Get("import_unique_hash").(string),
+	})
+	if err != nil {
+		return err
+	}
+
+	d.SetId(response.ObjectUUID)
+	log.Printf("The id of the imported marketplace application has been set to %v", response.ObjectUUID)
+
+	return resourceGridscaleMarketplaceApplicationRead(d, meta)
+}
---
+++ gridscale/resource_gridscale_marketplace_app_import_test.go
@@ -0,0 +1,131 @@
+package gridscale
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+
+	"github.com/gridscale/gsclient-go/v3"
+)
+
+func TestAccResourceGridscaleMarketplaceApplicationImport_Basic(t *testing.T) {
+	var object gsclient.MarketplaceApplication
+	name := fmt.Sprintf("object-%s", acctest.RandString(10))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckGridscaleMarketplaceApplicationImportDestroyCheck,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckResourceGridscaleMarketplaceApplicationImportConfig_basic(name),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckResourceGridscaleMarketplaceApplicationImportExists("gridscale_marketplace_application_import.foo", &object),
+					resource.TestCheckResourceAttr(
+						"gridscale_marketplace_application_import.foo", "name", name),
+				),
+			},
+			{
+				Config: testAccCheckResourceGridscaleMarketplaceApplicationImportConfig_basic_update(),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckResourceGridscaleMarketplaceApplicationImportExists("gridscale_marketplace_application_import.foo", &object),
+					resource.TestCheckResourceAttr(
+						"gridscale_marketplace_application_import.foo", "name", "newname"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckResourceGridscaleMarketplaceApplicationImportExists(n string, object *gsclient.MarketplaceApplication) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No object UUID is set")
+		}
+
+		client := testAccProvider.Meta().(*gsclient.Client)
+
+		id := rs.Primary.ID
+
+		foundObject, err := client.GetMarketplaceApplication(context.Background(), id)
+
+		if err != nil {
+			return err
+		}
+
+		if foundObject.Properties.ObjectUUID != id {
+			return fmt.Errorf("Object not found")
+		}
+
+		*object = foundObject
+
+		return nil
+	}
+}
+
+func testAccCheckGridscaleMarketplaceApplicationImportDestroyCheck(s *terraform.State) error {
+	client := testAccProvider.Meta().(*gsclient.Client)
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "gridscale_marketplace_application_import" {
+			continue
+		}
+
+		_, err := client.GetMarketplaceApplication(context.Background(), rs.Primary.ID)
+		if err != nil {
+			if requestError, ok := err.(gsclient.RequestError); ok {
+				if requestError.StatusCode != 404 {
+					return fmt.Errorf("Object %s still exists", rs.Primary.ID)
+				}
+			} else {
+				return fmt.Errorf("Unable to fetching object %s", rs.Primary.ID)
+			}
+		} else {
+			return fmt.Errorf("Object %s still exists", rs.Primary.ID)
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckResourceGridscaleMarketplaceApplicationImportConfig_basic(name string) string {
+	return fmt.Sprintf(`
+resource "gridscale_marketplace_application" "foo" {
+	name = "%s"
+	object_storage_path = "s3://testsnapshot/test.gz"
+	category = "CMS"
+	setup_cores = 1
+	setup_memory = 1
+	setup_storage_capacity = 1
+}
+
+resource "gridscale_marketplace_application_import" "foo" {
+	import_unique_hash = gridscale_marketplace_application.foo.unique_hash
+}
+`, name)
+}
+
+func testAccCheckResourceGridscaleMarketplaceApplicationImportConfig_basic_update() string {
+	return fmt.Sprintf(`
+resource "gridscale_marketplace_application" "foonew" {
+  	name   = "newname"
+	object_storage_path = "s3://testsnapshot/test.gz"
+	category = "CMS"
+	setup_cores = 2
+	setup_memory = 4
+	setup_storage_capacity = 5
+}
+resource "gridscale_marketplace_application_import" "foo" {
+	import_unique_hash = gridscale_marketplace_application.foonew.unique_hash
+}
+`)
+}
