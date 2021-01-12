e9dfe25a96b34254804e34312c6e4b31dd0deee4
Van Thong Nguyen <tom@gridscale.io>
2021-01-12 10:35:50 +0100

    update acc test
    Update acc test of PaaS
    
    update test acc
    
    update acc test
    
    update acc test

--- gridscale/resource_gridscale_paas_test.go
+++ gridscale/resource_gridscale_paas_test.go
@@ -38,7 +38,7 @@ func TestAccResourceGridscalePaaS_Basic(t *testing.T) {
 				),
 			},
 			{
-				Config: testAccCheckResourceGridscalePaaSConfig_forcenew_update(),
+				Config: testAccCheckResourceGridscalePaaSConfig_tmp_update(),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckResourceGridscalePaaSExists("gridscale_paas.foopaas", &object),
 					resource.TestCheckResourceAttr(
@@ -132,14 +132,31 @@ resource "gridscale_paas" "foopaas" {
 `)
 }
 
-func testAccCheckResourceGridscalePaaSConfig_forcenew_update() string {
+// TO DO: update `service_template_uuid` when the backend enables the option to
+// update `service_template_uuid`.
+func testAccCheckResourceGridscalePaaSConfig_tmp_update() string {
 	return fmt.Sprintf(`
 resource "gridscale_paas" "foopaas" {
   name = "newname"
-  service_template_uuid = "136c1446-13e0-4734-bdb6-ab0a15c1d680"
+  service_template_uuid = "8bcb216c-65ec-4c93-925d-1b8feaa5c2c5"
   resource_limit {
 	resource = "cores"
 	limit = 16
+  }
+  parameter {
+    param = "mysql_max_connections"
+    value = "2000"
+    type = "float"
+  }
+  parameter {
+    param = "mysql_query_cache"
+    value = "true"
+    type = "bool"
+  }
+  parameter {
+    param = "mysql_default_time_zone"
+    value = "UTC"
+    type = "string"
   }
 }
 `)
