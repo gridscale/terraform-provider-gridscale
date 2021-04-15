fe7224200b47d72c43154950dff5f042667ea9a5
Van Thong Nguyen <tom@gridscale.io>
2021-04-15 11:05:53 +0200

    set default timeouts of paas rs to 15 mins

--- gridscale/resource_gridscale_paas.go
+++ gridscale/resource_gridscale_paas.go
@@ -170,9 +170,9 @@ func resourceGridscalePaaS() *schema.Resource {
 			},
 		},
 		Timeouts: &schema.ResourceTimeout{
-			Create: schema.DefaultTimeout(30 * time.Minute),
-			Update: schema.DefaultTimeout(30 * time.Minute),
-			Delete: schema.DefaultTimeout(30 * time.Minute),
+			Create: schema.DefaultTimeout(15 * time.Minute),
+			Update: schema.DefaultTimeout(15 * time.Minute),
+			Delete: schema.DefaultTimeout(15 * time.Minute),
 		},
 	}
 }
