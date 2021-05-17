d0333198386d5edac8ddb71afe34527e1554ed39
Van Thong Nguyen <tom@gridscale.io>
2021-05-17 16:03:47 +0200

    add sql server rs acctest

---
+++ gridscale/resource_gridscale_mssql_test.go
@@ -0,0 +1,61 @@
+package gridscale
+
+import (
+	"fmt"
+
+	"github.com/gridscale/gsclient-go/v3"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"testing"
+)
+
+func TestAccResourceGridscaleMSSQLServer_Basic(t *testing.T) {
+	var object gsclient.PaaSService
+	name := fmt.Sprintf("postgres-%s", acctest.RandString(10))
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckResourceGridscalePaaSDestroyCheck,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckResourceGridscaleMSSQLServerConfig_basic(name),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckResourceGridscalePaaSExists("gridscale_sqlserver.test", &object),
+					resource.TestCheckResourceAttr(
+						"gridscale_sqlserver.test", "name", name),
+				),
+			},
+			{
+				Config: testAccCheckResourceGridscaleMSSQLServerConfig_basic_update(),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckResourceGridscalePaaSExists("gridscale_sqlserver.test", &object),
+					resource.TestCheckResourceAttr(
+						"gridscale_sqlserver.test", "name", "newname"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckResourceGridscaleMSSQLServerConfig_basic(name string) string {
+	return fmt.Sprintf(`
+resource "gridscale_sqlserver" "test" {
+	name = "%s"
+	release = "2019"
+	performance_class = "standard"
+}
+`, name)
+}
+
+func testAccCheckResourceGridscaleMSSQLServerConfig_basic_update() string {
+	return fmt.Sprintf(`
+resource "gridscale_sqlserver" "test" {
+	name = "newname"
+	release = "2019"
+	performance_class = "standard"
+	labels = ["test"]
+}
+`)
+}
