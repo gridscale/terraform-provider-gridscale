3ccbd737726f9c86963d69bff404719e077a95c5
Van Thong Nguyen <tom@gridscale.io>
2021-06-10 10:45:36 +0200

    add field 'host' to memcached rs

--- gridscale/resource_gridscale_memcached.go
+++ gridscale/resource_gridscale_memcached.go
@@ -100,6 +100,10 @@ func resourceGridscaleMemcached() *schema.Resource {
 							Type:     schema.TypeString,
 							Computed: true,
 						},
+						"host": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
 						"port": {
 							Type:     schema.TypeInt,
 							Computed: true,
@@ -213,10 +217,11 @@ func resourceGridscaleMemcachedRead(d *schema.ResourceData, meta interface{}) er
 
 	//Get listen ports
 	listenPorts := make([]interface{}, 0)
-	for _, value := range props.ListenPorts {
+	for host, value := range props.ListenPorts {
 		for k, portValue := range value {
 			port := map[string]interface{}{
 				"name": k,
+				"host": host,
 				"port": portValue,
 			}
 			listenPorts = append(listenPorts, port)
