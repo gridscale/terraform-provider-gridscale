9d8a175b94b030af24314cbe6f96e55a01db2599
Van Thong Nguyen <tom@gridscale.io>
2021-09-02 00:39:47 +0200

    update network resource's acctest

--- gridscale/resource_gridscale_network_test.go
+++ gridscale/resource_gridscale_network_test.go
@@ -27,6 +27,14 @@ func TestAccResourceGridscaleNetwork_Basic(t *testing.T) {
 					testAccCheckResourceGridscaleNetworkExists("gridscale_network.foo", &object),
 					resource.TestCheckResourceAttr(
 						"gridscale_network.foo", "name", name),
+					resource.TestCheckResourceAttr(
+						"gridscale_network.foo", "dhcp_active", "true"),
+					resource.TestCheckResourceAttr(
+						"gridscale_network.foo", "dhcp_gateway", "192.168.121.1"),
+					resource.TestCheckResourceAttr(
+						"gridscale_network.foo", "dhcp_dns", "192.168.121.2"),
+					resource.TestCheckResourceAttr(
+						"gridscale_network.foo", "dhcp_reserved_subnet.#", "1"),
 				),
 			},
 			{
@@ -37,6 +45,14 @@ func TestAccResourceGridscaleNetwork_Basic(t *testing.T) {
 						"gridscale_network.foo", "name", "newname"),
 					resource.TestCheckResourceAttr(
 						"gridscale_network.foo", "l2security", "true"),
+					resource.TestCheckResourceAttr(
+						"gridscale_network.foo", "dhcp_active", "true"),
+					resource.TestCheckResourceAttr(
+						"gridscale_network.foo", "dhcp_gateway", "192.168.122.1"),
+					resource.TestCheckResourceAttr(
+						"gridscale_network.foo", "dhcp_dns", "192.168.122.2"),
+					resource.TestCheckResourceAttr(
+						"gridscale_network.foo", "dhcp_reserved_subnet.#", "1"),
 				),
 			},
 		},
@@ -103,15 +119,25 @@ func testAccCheckResourceGridscaleNetworkConfig_basic(name string) string {
 	return fmt.Sprintf(`
 resource "gridscale_network" "foo" {
   name   = "%s"
+  dhcp_active = true
+  dhcp_gateway = "192.168.121.1"
+  dhcp_dns = "192.168.121.2"
+  dhcp_range = "192.168.121.0/27"
+  dhcp_reserved_subnet = ["192.168.121.0/31"]
 }
 `, name)
 }
 
 func testAccCheckResourceGridscaleNetworkConfig_basic_update() string {
-	return fmt.Sprintf(`
+	return fmt.Sprint(`
 resource "gridscale_network" "foo" {
   name   = "newname"
   l2security = true
+  dhcp_active = true
+  dhcp_gateway = "192.168.122.1"
+  dhcp_dns = "192.168.122.2"
+  dhcp_range = "192.168.122.0/27"
+  dhcp_reserved_subnet = ["192.168.122.0/31"]
 }
 `)
 }
