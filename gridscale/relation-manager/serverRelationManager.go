8698e30a648abe52c663d311c1d28e20dbf3d75e
Van Thong Nguyen <tom@gridscale.io>
2022-01-26 15:24:01 +0100

    updating hw profile requires server to be off

--- gridscale/relation-manager/serverRelationManager.go
+++ gridscale/relation-manager/serverRelationManager.go
@@ -235,7 +235,7 @@ func (c *ServerRelationManger) IsShutdownRequired(ctx context.Context) bool {
 	var shutdownRequired bool
 	d := c.getData()
 	hasServerNetListChange := c.hasServerNetworkListChanged(ctx)
-	if d.HasChanges("cores", "memory", "ipv4", "ipv6") || hasServerNetListChange {
+	if d.HasChanges("cores", "memory", "ipv4", "ipv6", "hardware_profile") || hasServerNetListChange {
 		shutdownRequired = true
 	}
 	return shutdownRequired
