1888e74af4689cf30444dbdd5c004113d594c602
Van Thong Nguyen <tom@gridscale.io>
2022-04-08 15:28:19 +0200

    add private net support to memcached rs

--- gridscale/resource_gridscale_memcached.go
+++ gridscale/resource_gridscale_memcached.go
@@ -114,19 +114,26 @@ func resourceGridscaleMemcached() *schema.Resource {
 			"security_zone_uuid": {
 				Type:        schema.TypeString,
 				Description: "Security zone UUID linked to Memcached service.",
+				Deprecated:  "Security zone is deprecated for gridSQL, gridStore, and gridFs. Please consider to use private network instead.",
 				Optional:    true,
 				ForceNew:    true,
 				Computed:    true,
 			},
 			"network_uuid": {
 				Type:        schema.TypeString,
-				Description: "Network UUID containing security zone.",
+				Description: "The UUID of the network that the service is attached to.",
+				Optional:    true,
 				Computed:    true,
 			},
 			"service_template_uuid": {
 				Type:        schema.TypeString,
 				Description: "PaaS service template that Memcached service uses.",
 				Computed:    true,
+			},
+			"service_template_category": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The template service's category used to create the service.",
 			},
 			"usage_in_minutes": {
 				Type:        schema.TypeInt,
@@ -199,8 +206,14 @@ func resourceGridscaleMemcachedRead(d *schema.ResourceData, meta interface{}) er
 	if err = d.Set("security_zone_uuid", props.SecurityZoneUUID); err != nil {
 		return fmt.Errorf("%s error setting security_zone_uuid: %v", errorPrefix, err)
 	}
+	if err = d.Set("network_uuid", props.NetworkUUID); err != nil {
+		return fmt.Errorf("%s error setting network_uuid: %v", errorPrefix, err)
+	}
 	if err = d.Set("service_template_uuid", props.ServiceTemplateUUID); err != nil {
 		return fmt.Errorf("%s error setting service_template_uuid: %v", errorPrefix, err)
+	}
+	if err = d.Set("service_template_category", props.ServiceTemplateCategory); err != nil {
+		return fmt.Errorf("%s error setting service_template_category: %v", errorPrefix, err)
 	}
 	if err = d.Set("usage_in_minutes", props.UsageInMinutes); err != nil {
 		return fmt.Errorf("%s error setting usage_in_minutes: %v", errorPrefix, err)
@@ -245,7 +258,11 @@ func resourceGridscaleMemcachedRead(d *schema.ResourceData, meta interface{}) er
 		return fmt.Errorf("%s error setting labels: %v", errorPrefix, err)
 	}
 
-	//Get all available networks
+	// Look for security zone's network that the PaaS service is connected to
+	// (if the paas is connected to security zone. O.w skip)
+	if props.SecurityZoneUUID == "" {
+		return nil
+	}
 	networks, err := client.GetNetworkList(context.Background())
 	if err != nil {
 		return fmt.Errorf("%s error getting networks: %v", errorPrefix, err)
@@ -281,9 +298,15 @@ func resourceGridscaleMemcachedCreate(d *schema.ResourceData, meta interface{}) 
 		Name:                    d.Get("name").(string),
 		PaaSServiceTemplateUUID: templateUUID,
 		Labels:                  convSOStrings(d.Get("labels").(*schema.Set).List()),
-		PaaSSecurityZoneUUID:    d.Get("security_zone_uuid").(string),
 	}
-
+	networkUUIDInf, isNetworkSet := d.GetOk("network_uuid")
+	if isNetworkSet {
+		requestBody.NetworkUUID = networkUUIDInf.(string)
+	}
+	// If network_uuid is set, skip setting security_zone_uuid.
+	if secZoneUUIDInf, ok := d.GetOk("security_zone_uuid"); ok && !isNetworkSet {
+		requestBody.PaaSSecurityZoneUUID = secZoneUUIDInf.(string)
+	}
 	if val, ok := d.GetOk("max_core_count"); ok {
 		limits := []gsclient.ResourceLimit{
 			{
@@ -314,7 +337,9 @@ func resourceGridscaleMemcachedUpdate(d *schema.ResourceData, meta interface{}) 
 		Name:   d.Get("name").(string),
 		Labels: &labels,
 	}
-
+	if d.HasChange("network_uuid") {
+		requestBody.NetworkUUID = d.Get("network_uuid").(string)
+	}
 	// Only update templateUUID, when `release` or `performance_class` is changed
 	if d.HasChange("release") || d.HasChange("performance_class") {
 		// Get memcached template UUID
