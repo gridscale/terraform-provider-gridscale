cc2117e1a6cc4cb8c8a9a5a1e7e4f7eb4e03a3ee
Van Thong Nguyen <tom@gridscale.io>
2022-04-08 15:30:49 +0200

    add private net support to mysql rs

--- gridscale/resource_gridscale_mysql.go
+++ gridscale/resource_gridscale_mysql.go
@@ -175,19 +175,26 @@ func resourceGridscaleMySQL() *schema.Resource {
 			"security_zone_uuid": {
 				Type:        schema.TypeString,
 				Description: "Security zone UUID linked to MySQL service.",
+				Deprecated:  "Security zone is deprecated for gridSQL, gridStore, and gridFs. Please consider to use private network instead.",
 				Optional:    true,
 				ForceNew:    true,
 				Computed:    true,
 			},
 			"network_uuid": {
 				Type:        schema.TypeString,
-				Description: "Network UUID containing security zone.",
+				Description: "The UUID of the network that the service is attached to.",
+				Optional:    true,
 				Computed:    true,
 			},
 			"service_template_uuid": {
 				Type:        schema.TypeString,
 				Description: "PaaS service template that MySQL service uses.",
 				Computed:    true,
+			},
+			"service_template_category": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The template service's category used to create the service.",
 			},
 			"usage_in_minutes": {
 				Type:        schema.TypeInt,
@@ -257,8 +264,14 @@ func resourceGridscaleMySQLRead(d *schema.ResourceData, meta interface{}) error 
 			return fmt.Errorf("%s error setting password: %v", errorPrefix, err)
 		}
 	}
+	if err = d.Set("network_uuid", props.NetworkUUID); err != nil {
+		return fmt.Errorf("%s error setting network_uuid: %v", errorPrefix, err)
+	}
 	if err = d.Set("security_zone_uuid", props.SecurityZoneUUID); err != nil {
 		return fmt.Errorf("%s error setting security_zone_uuid: %v", errorPrefix, err)
+	}
+	if err = d.Set("service_template_category", props.ServiceTemplateCategory); err != nil {
+		return fmt.Errorf("%s error setting service_template_category: %v", errorPrefix, err)
 	}
 	if err = d.Set("service_template_uuid", props.ServiceTemplateUUID); err != nil {
 		return fmt.Errorf("%s error setting service_template_uuid: %v", errorPrefix, err)
@@ -337,7 +350,11 @@ func resourceGridscaleMySQLRead(d *schema.ResourceData, meta interface{}) error 
 		return fmt.Errorf("%s error setting labels: %v", errorPrefix, err)
 	}
 
-	//Get all available networks
+	// Look for security zone's network that the PaaS service is connected to
+	// (if the paas is connected to security zone. O.w skip)
+	if props.SecurityZoneUUID == "" {
+		return nil
+	}
 	networks, err := client.GetNetworkList(context.Background())
 	if err != nil {
 		return fmt.Errorf("%s error getting networks: %v", errorPrefix, err)
@@ -373,9 +390,15 @@ func resourceGridscaleMySQLCreate(d *schema.ResourceData, meta interface{}) erro
 		Name:                    d.Get("name").(string),
 		PaaSServiceTemplateUUID: templateUUID,
 		Labels:                  convSOStrings(d.Get("labels").(*schema.Set).List()),
-		PaaSSecurityZoneUUID:    d.Get("security_zone_uuid").(string),
 	}
-
+	networkUUIDInf, isNetworkSet := d.GetOk("network_uuid")
+	if isNetworkSet {
+		requestBody.NetworkUUID = networkUUIDInf.(string)
+	}
+	// If network_uuid is set, skip setting security_zone_uuid.
+	if secZoneUUIDInf, ok := d.GetOk("security_zone_uuid"); ok && !isNetworkSet {
+		requestBody.PaaSSecurityZoneUUID = secZoneUUIDInf.(string)
+	}
 	if val, ok := d.GetOk("max_core_count"); ok {
 		limits := []gsclient.ResourceLimit{
 			{
@@ -419,7 +442,9 @@ func resourceGridscaleMySQLUpdate(d *schema.ResourceData, meta interface{}) erro
 		Name:   d.Get("name").(string),
 		Labels: &labels,
 	}
-
+	if d.HasChange("network_uuid") {
+		requestBody.NetworkUUID = d.Get("network_uuid").(string)
+	}
 	// Only update templateUUID, when `release` or `performance_class` is changed
 	if d.HasChange("release") || d.HasChange("performance_class") {
 		// Get mysql template UUID
