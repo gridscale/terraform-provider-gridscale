903bae31034e7503e1a10ec2748e11a9b11b787b
Van Thong Nguyen <nvthongswansea@gmail.com>
2023-05-19 17:30:09 +0200

    allow comment & user_uuid in obj storage key
    Also add obj storage key update operation to update comment

--- gridscale/datasource_gridscale_objectstorage.go
+++ gridscale/datasource_gridscale_objectstorage.go
@@ -19,6 +19,19 @@ func dataSourceGridscaleObjectStorage() *schema.Resource {
 				Description:  "ID of a resource",
 				ValidateFunc: validation.NoZeroValues,
 			},
+			"comment": {
+				Type:        schema.TypeString,
+				Description: "Comment for the access_key.",
+				Computed:    true,
+			},
+			"user_uuid": {
+				Type: schema.TypeString,
+				Description: `If a user_uuid is set, a user-specific key will get created. 
+				If no user_uuid is set along a user with write-access to the contract will still only create 
+				a user-specific key for themselves while a user with admin-access to the contract will create 
+				a contract-level admin key.`,
+				Computed: true,
+			},
 			"access_key": {
 				Type:        schema.TypeString,
 				Description: "The object storage secret_key.",
@@ -47,6 +60,12 @@ func dataSourceGridscaleObjectStorageRead(d *schema.ResourceData, meta interface
 	}
 
 	d.SetId(objectStorage.Properties.AccessKey)
+	if err = d.Set("comment", objectStorage.Properties.Comment); err != nil {
+		return fmt.Errorf("%s error setting comment: %v", errorPrefix, err)
+	}
+	if err = d.Set("user_uuid", objectStorage.Properties.UserUUID); err != nil {
+		return fmt.Errorf("%s error setting user_uuid: %v", errorPrefix, err)
+	}
 	if err = d.Set("access_key", objectStorage.Properties.AccessKey); err != nil {
 		return fmt.Errorf("%s error setting access_key: %v", errorPrefix, err)
 	}
